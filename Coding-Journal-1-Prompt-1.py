ba872a4bc89f3e0c72ec365eec7c19c73071f3cd
Aismagil <102846105+Aismagil@users.noreply.github.com>
2022-04-22 02:16:35 -0700

    Session 1 prompt for Coding Journal 1.

---
+++ Coding-Journal-1-Prompt-1.py
@@ -0,0 +1,27 @@
+#This program will write 
+#some basic introductory information!
+
+
+#This function tells the 
+#computer to print
+#personal information like
+#name, preferred pronouns,
+#and sentences about favorite
+#movie and food
+def PrintPersonalInformation():
+    print("Amina Ismagilova")
+    print("she, her")
+    print("My favorite movie is Doctor Strange.")
+    print("My favorite food is almost anything sweet.")
+
+
+#This defines our main()
+#function for our program
+def main():
+    PrintPersonalInformation()
+
+
+#When we run the program,
+#this executes first
+if __name__=="__main__":
+    main()
